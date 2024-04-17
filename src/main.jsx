0cd7a481e47bd0b4e6b9be3380c23ec49b23d455
Jaison <jaisonschmidt@gmail.com>
2024-04-16 23:04:43 -0300

    feat: Cria pagina de ver post

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import PageHome from './pages/PageHome';
+import PagePost from './pages/PagePost';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PageHome />
+    <PagePost />
   </React.StrictMode>,
 )
