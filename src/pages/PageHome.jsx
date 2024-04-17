4c0098f7a2642c104eb60048cfec303586e8468c
Jaison <jaisonschmidt@gmail.com>
2024-04-16 22:13:02 -0300

    feat: Integra com Contentful - POSTS

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
 import Post from "../components/Post";
 import LayoutDefault from "../layout/LayoutDefault";
+import { client } from "../util/createClient";
 
 function PageHome() {
+    const [posts, setPosts] = useState([]);
+    const [feedbackPosts, setFeedbackPosts] = useState('Carregando posts...');
+
+    const getPosts = async () => {
+        try {
+            const response = await client.getEntries({
+                content_type: 'fiapBlogPost',
+                limit: 5,
+                order: "-sys.createdAt"
+            });
+    
+            setPosts(response.items);
+        } catch (error) {
+            setFeedbackPosts('Erro ao carregar os posts, run to the hills!');
+        }
+    };
+
+    useEffect(() => {
+        getPosts();
+    }, []); // ciclo de vida - no onLoad do componente
+    
     return (
         <LayoutDefault>
             <div className="container">
@@ -9,11 +32,18 @@ function PageHome() {
                     <main className="col-md-8">
                         <h2 className="my-3">Área dos posts</h2>
 
-                        <Post 
-                            title={'Primeiro Post'} 
-                            description={'Descrição'} 
-                            slug={'primeiro-post'} 
-                        />
+                        {posts.length === 0 && (
+                            <p>{feedbackPosts}</p>
+                        )}
+
+                        {posts.map((post) => (
+                            <Post
+                                key={post.sys.id}
+                                title={post.fields.postTitle}
+                                description={post.fields.postDescription}
+                                slug={post.fields.postSlug}
+                            />
+                        ))}
 
                         <a href="#" className="btn btn-primary">
                             Ver todos os posts
