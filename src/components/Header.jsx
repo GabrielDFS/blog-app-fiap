aa62c33fa580fef8adad9b8f0cf2c86245fc5bdc
Jaison <jaisonschmidt@gmail.com>
2024-04-23 20:28:14 -0300

    feat: Change header link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
     <nav className="navbar bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">My Blog</a>
+        <Link to="/" className="navbar-brand">My Blog</Link>
       </div>
     </nav>
   );
